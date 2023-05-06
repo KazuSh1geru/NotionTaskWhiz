b5b079d11f7b8069ded586dea18e9ca1c6a72324
Takakusagi Kazushi <kusagi@TakakusaginoMBP.usen.ad.jp>
2023-05-06 17:14:12 +0900

    taskのステータス変更まで

--- src/notion_task_manager.py
+++ src/notion_task_manager.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import os
 import json
 import requests
@@ -8,7 +9,7 @@ dotenv.load_dotenv()
 
 integration_token = os.getenv("NOTION_INTEGRATION_TOKEN")
 target_task_database_id = os.getenv("TARGET_TASK_DATABASE_ID")
-sample_page_id = os.getenv("SAMPLE_PAGE_ID")
+# sample_page_id = os.getenv("SAMPLE_PAGE_ID")
 
 
 headers = {
@@ -17,8 +18,6 @@ headers = {
     "Content-Type": "application/json",
     "Authorization": "Bearer " + integration_token,
 }
-
-payload = {"page_size": 100}
 
 
 def create_task(task_name, status=TASK_STATUS["not_started"]):
@@ -35,44 +34,42 @@ def create_task(task_name, status=TASK_STATUS["not_started"]):
     if response.status_code == 200:
         res = response.text
         # ここでデータを処理する
-        print(res)
+        pprint(res)
     else:
-        print("エラーが発生しました。ステータスコード:", response.status_code)
+        pprint("エラーが発生しました。ステータスコード:", response.status_code)
 
 
+# task_name から page_id を取得する
 def find_task(task_name):
+    payload = {"filter": {"property": "Name", "title": {"equals": task_name}}}
     url = f"https://api.notion.com/v1/databases/{target_task_database_id}/query"
     response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        data = response.text
+        res = response.json()
         # ここでデータを処理する
-        print(data)
+        page_id = res["results"][0]["id"]
+        pprint(page_id)
+        return page_id
     else:
         print("エラーが発生しました。ステータスコード:", response.status_code)
 
 
-# data = {
-#     "properties": {
-#         "Status": {
-#             # "status": { "name": "In progress" }
-#             "status": {"name": "Done"}
-#         }
-#     }
-# }
-
-
-def test_status_change(sample_page_id, data):
-    url = f"https://api.notion.com/v1/pages/{sample_page_id}"
+# task_name から page_id を取得して、ステータスを更新する
+def status_change(task_name, status=TASK_STATUS["done"]):
+    page_id = find_task(task_name=task_name)
+    data = {"properties": {"Status": {"status": {"name": status}}}}
+    url = f"https://api.notion.com/v1/pages/{page_id}"
     response = requests.patch(url, headers=headers, data=json.dumps(data))
 
     if response.status_code == 200:
-        print("更新が成功しました")
+        pprint("更新が成功しました")
     else:
-        print("エラーが発生しました。ステータスコード:", response.status_code)
+        pprint("エラーが発生しました。ステータスコード:", response.status_code)
 
 
 def test_response():
+    payload = {"page_size": 100}
     url = f"https://api.notion.com/v1/databases/{target_task_database_id}/query"
     response = requests.post(url, json=payload, headers=headers)
 
@@ -80,13 +77,15 @@ def test_response():
     # response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.text
+        data = response.json()
         # ここでデータを処理する
-        print(data)
+        pprint(data)
     else:
-        print("エラーが発生しました。ステータスコード:", response.status_code)
+        pprint("エラーが発生しました。ステータスコード:", response.status_code)
 
 
 if __name__ == "__main__":
-    create_task(data)
+    # create_task(data)
     # test_response()
+    # find_task("ティッシュ買う")
+    status_change("ティッシュ買う")
