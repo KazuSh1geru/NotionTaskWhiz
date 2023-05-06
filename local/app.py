fcca7bdf298e415dbf4ce0852fcb9511cdb16aed
Takakusagi Kazushi <kusagi@TakakusaginoMBP.usen.ad.jp>
2023-05-06 14:34:21 +0900

    setup

--- local/app.py
+++ local/app.py
@@ -2,6 +2,7 @@ import os
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from src.notion_task_manager import create_task
 
 from dotenv import load_dotenv
 
@@ -21,8 +22,21 @@ def hello_command(ack, body):
 @app.event("app_mention")
 def event_test(say):
     say("Hi there!")
+    data = {
+        "parent": { "database_id": os.getenv("TARGET_TASK_DATABASE_ID") },
+        "properties": {
+            "Name": {
+            "title": [
+                { "text": { "content": "test_record" } }
+            ]
+            },
+            "Status": {
+            "status": { "name": "In progress" }
+            }
+        }
+    }
+    create_task(data)
 
 
 if __name__ == "__main__":
-    print(os.getenv("SLACK_BOT_TOKEN"))
     SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name="src",
+    version="0.1.0",
+    description="",
+    author="",
+    packages=["src"],
+    package_dir={
+        "src": "src",
+    }
+)
