bab70eaeee211200a86dba8263ad3ca24725a38a
Takakusagi Kazushi <kusagi@TakakusaginoMBP.usen.ad.jp>
2023-05-07 15:39:20 +0900

    create_task modal

--- local/app.py
+++ local/app.py
@@ -15,12 +15,55 @@ app = App(token=os.getenv("SLACK_BOT_TOKEN"))
 
 
 @app.command("/create-task")
-def create_task_command(ack, body, say):
+def create_task_command(ack, body, logger):
+    client = app.client
+    trigger_id = body["trigger_id"]
+    view = {
+        "title": {"type": "plain_text", "text": "タスクの新規登録 :pencil:", "emoji": True},
+        "submit": {"type": "plain_text", "text": "送信", "emoji": True},
+        "type": "modal",
+        "close": {"type": "plain_text", "text": "キャンセル", "emoji": True},
+        "blocks": [
+            {
+                "type": "input",
+                "block_id": "input-title",
+                "element": {
+                    "type": "plain_text_input",
+                    "action_id": "input",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "件名を入力してください",
+                        "emoji": True,
+                    },
+                },
+                "label": {"type": "plain_text", "text": "件名", "emoji": True},
+                "optional": False,
+            }
+        ],
+    }
+    try:
+        response = client.views_open(trigger_id=trigger_id, view=view)
+        logger.info(response)
+
+        # モーダルで送信ボタンが押された時の処理
+        @app.view("")
+        def handle_create_task(ack, body, logger):
+            logger.info(body)
+            try:
+                task_name = body["view"]["state"]["values"]["input-title"]["input"][
+                    "value"
+                ]
+                create_task(task_name=task_name)
+                ack()
+                app.client.chat_postMessage(
+                    channel=os.getenv("SLACK_CHANNEL_ID"), text=f"{task_name} を作成しました！"
+                )
+            except Exception as e:
+                logger.error(str(e))
+
+    except Exception as e:
+        print(e)
     ack()
-    print(body)
-    task_name = body["text"]
-    create_task(task_name=task_name)
-    say("Task: " + task_name + " を作成しました")
 
 
 @app.command("/done-task")
